114bfb678432fb7a4f76ee8ae987fc1381ce876f
kat-hi <katharina.sachs@beuth-hochschule.de>
2020-01-22 00:00:04 +0100

    wip

--- scripts/json_converter.py
+++ scripts/json_converter.py
@@ -4,20 +4,34 @@ import os
 import re
 
 regex = "(\D)*"
-colnames = []
-values = []
-
-for filename in os.listdir('../data/'):
-	colnames.append(re.search(regex, filename).group())
-	file = open('../data/'+filename, 'r')
-	data = json.load(file)
-	results = data['results'][0]['series'][0]['values']
-	file.close()
+colnames_cum = []
+data_cum = {}
 
 
-'''
-with open('../data/data.csv', 'w') as file:
-	for item in res:
-		file.writelines(str(item[8]))
-file.close()
-'''
+def extract_values_from_json():
+	for file in os.listdir('../data/'):
+		colname = re.search(regex, file).group()
+		colnames_cum.append(colname)
+
+		filename = '../data/'+file
+		print(filename)
+		f = open(filename, 'r')
+		dat_temp = json.load(f)
+		results = dat_temp['results'][0]['series'][0]['values']
+
+		col_values = []
+		for item in results:
+			col_values.append(item[8])
+		data_cum[colname] = col_values
+
+
+def write_csv_data_file():
+	with open('../ressources/data.csv', 'w') as file:
+		for key_col in colnames_cum:
+			file.write(key_col + " " + str(data_cum[key_col].strip('[]')))
+		file.close()
+
+
+if __name__ == '__main__':
+	extract_values_from_json()
+	write_csv_data_file()
