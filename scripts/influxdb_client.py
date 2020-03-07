ba9a7f5e4a86c30a055df655fbf5ee1bd6695d38
kat-hi <s77518@beuth-hochschule.de>
2020-03-07 19:08:46 +0100

    concurrency and new time query

--- scripts/influxdb_client.py
+++ scripts/influxdb_client.py
@@ -1,7 +1,9 @@
 import argparse
 import pandas as pd
 from influxdb import DataFrameClient
-
+import threading
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 HOST = '127.0.0.1'
 PORT = 8086
@@ -15,8 +17,7 @@ params = {
 
 def parse_args():
     """Parse the args from main."""
-    parser = argparse.ArgumentParser(
-        description='example code to play with InfluxDB')
+    parser = argparse.ArgumentParser(description='example code to play with InfluxDB')
     parser.add_argument('--host', type=str, required=False,
                         default='localhost',
                         help='hostname of InfluxDB http API')
@@ -25,20 +26,33 @@ def parse_args():
     return parser.parse_args()
 
 
+def data_preprocessing(predictor, colnames, client):
+    print('predictor: ' + predictor)
+
+    resp = client.query("select %s,%s from %s WHERE time >= '2020-03-02T10:00:00.00Z' AND time <= '2020-03-04T10:00:00.00Z'" % (colnames[0], colnames[1], predictor))
+    if not resp:
+        print('no data for predictor: ' + predictor)
+    else:
+        df = pd.DataFrame(resp[predictor], columns=colnames)
+        df['date'] = df.index.date
+        df['day'] = df.index.day
+        df['time'] = df.index.time
+
+        sns.set(rc={'figure.figsize': (11, 4)})
+        if predictor is not 'up':
+            df['value'].plot(linewidth=0.5)
+            plt.show()
+
 def main(host='localhost', port=8086):
     client = DataFrameClient(host, port, database=DB)
 
-    # query variables "up" and "node_cpu_frequency_hertz"
-    resp_up = client.query("select host, value from up WHERE time > now() - 1h")
-    resp_node_cpu_frequency_hertz = client.query(("select cpu, host, value from node_cpu_frequency_hertz WHERE time > now() - 1h"))
+    t1 = threading.Thread(target=data_preprocessing, args=('up', ['host', 'value'], client))
+    t2 = threading.Thread(target=data_preprocessing, args=('node_cpu_frequency_hertz', ['host', 'value'], client))
 
-    # create pandas dataframe
-    df_up = pd.DataFrame(resp_up['up'], columns=['host', 'value'])
-    df_node_cpu_frequency_hertz = pd.DataFrame(resp_node_cpu_frequency_hertz['node_cpu_frequency_hertz'],
-                                               columns=['cpu', 'host', 'value'])
-    # access timestamps with index parameter
-    for index in df_node_cpu_frequency_hertz.iterrows():
-        print(index)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
 
 # @TODO index matching of different variables and time series analysis
 # @TODO running all in multiple threads to parallelize these processes
