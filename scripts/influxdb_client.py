54e300d2b7cff5580db5c311d94f16474d33c921
kat-hi <s77518@beuth-hochschule.de>
2020-03-14 12:54:37 +0100

    refactoring

--- scripts/influxdb_client.py
+++ scripts/influxdb_client.py
@@ -5,16 +5,12 @@ import threading
 import queue
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import logging as log
 HOST = '127.0.0.1'
 PORT = 8086
 DB = 'prometheus'
 
-params = {
-    "chunked": True,
-    "chunk_size": 10
-}
-
+log.basicConfig(level=log.INFO, format='%(message)s')
 
 def parse_args():
     """Parse the args from main."""
@@ -24,39 +20,60 @@ def parse_args():
     return parser.parse_args()
 
 
-def data_preprocessing(predictor, colnames, client):
-    print('predictor: ' + predictor)
+def data_exploration(predictor, colnames, client):
+    log.info('\n predictor: ' + predictor)
 
     resp = client.query(
-        "select mean(%s) from %s WHERE time >= '2020-03-09T08:00:00.00Z' AND time <= '2020-03-12T20:00:00.00Z' GROUP BY time(5s)" % (
-            colnames[0], predictor))
+        "select mean(%s) from %s WHERE time >= '2020-03-09T08:00:00.00Z' AND time <= '2020-03-12T20:00:00.00Z' "
+        "GROUP BY time(60s)" % (colnames[0], predictor))
 
     if not resp:
-        print('no data for predictor: ' + predictor)
+        log.info('no data for predictor: ' + predictor + ' and queried colname: ' + colnames[0])
     else:
         df = pd.DataFrame(resp[predictor], columns=['mean'])
+
+        # PLOTTING
+        axes = df['mean'].plot(marker=".", linewidth=0.05, alpha=0.4, color="g", figsize=(25, 4))
+        axes.set_ylabel(predictor)
+        axes.set_xlabel('time')
+        # plt.show()
+
+        # DATACLEANING
+        df = df.dropna()
+
+        # DATAEXPLORATION
+        ## PRINT MEAN, STD AND VAR
+        #log.info('mean: ' + str(float(df.mean())))
+        #log.info('sd: ' + str(float(df.std())))
+        log.info('mean:  ' + str(float(df.mean(axis=0))))
+        log.info('std: ' + str(float(df.std(axis=0))))
+        log.info('min: ' + str(float(df.min())))
+        log.info('max: ' + str(float(df.max())))
+        log.info('n: ' + str(float(df.count())))
+        log.info('')
+
+        # DATAFRAMES TO CSV
+        df.to_csv(r'../data/' + predictor, index=True, header=True)
 
         df['date'] = df.index.date
         df['day'] = df.index.day
         df['time'] = df.index.time
         df['index'] = df.index
-
-        axes = df['mean'].plot(marker=".", linewidth=0.05, alpha=0.4, color="g", figsize=(25, 4))
-        axes.set_ylabel(predictor)
-        axes.set_xlabel('time')
-        plt.show()
-        df = df.dropna()
-
         return df
 
 
 def main(host='localhost', port=8086):
     client = DataFrameClient(host, port, database=DB)
-    df_up = data_preprocessing('up', ['value'], client)
-    df_ceph_osd_op_r = data_preprocessing('ceph_osd_op_r', ['value'], client)
-    df_ceph_osd_op_w = data_preprocessing('ceph_osd_op_w', ['value'], client)
-    df_ceph_osd_op_rw = data_preprocessing('ceph_osd_op_rw', ['value'], client)
 
+    # CREATE DATAFRAMES
+    df_up = data_exploration('up', ['value'], client)
+    df_ceph_osd_op_r = data_exploration('ceph_osd_op_r', ['value'], client)
+    df_ceph_osd_op_w = data_exploration('ceph_osd_op_w', ['value'], client)
+    df_ceph_osd_op_rw = data_exploration('ceph_osd_op_rw', ['value'], client)
+
+    # DATAEXPLORATION step by step
+    ## PRINT MEAN; STD AND VAR
+    '''
     print(str(df_up.values.mean()) + ' ' + str(df_up.values.std()) + ' ' + str(df_up.var()))
     print(str(df_ceph_osd_op_r.values.mean()) + ' ' + str(df_ceph_osd_op_r.values.std()) + ' ' + str(
         df_ceph_osd_op_r.var()))
@@ -65,12 +82,16 @@ def main(host='localhost', port=8086):
     print(str(df_ceph_osd_op_w.values.mean()) + ' ' + str(df_ceph_osd_op_w.values.std()) + ' ' + str(
         df_ceph_osd_op_w.var()))
 
-    # DATAFRAMES TO CSV
-    # df_ceph_osd_op_rw.to_csv(r'../data/ceph_osd_op_rw.csv', index=True, header=True)
-    # df_ceph_osd_op_r.to_csv(r'../data/ceph_osd_op_r.csv', index=True, header=True)
-    # df_ceph_osd_op_w.to_csv(r'../data/ceph_osd_op_w.csv', index=True, header=True)
-    df_up.to_csv(r'../data/up.csv', index=True, header=True)
+    print(df_up.count())
+    '''
 
+    # DATAFRAMES TO CSV
+    '''
+    df_ceph_osd_op_rw.to_csv(r'../data/ceph_osd_op_rw.csv', index=True, header=True)
+    df_ceph_osd_op_r.to_csv(r'../data/ceph_osd_op_r.csv', index=True, header=True)
+    df_ceph_osd_op_w.to_csv(r'../data/ceph_osd_op_w.csv', index=True, header=True)
+    df_up.to_csv(r'../data/up.csv', index=True, header=True)
+    '''
 
 '''
   threading
