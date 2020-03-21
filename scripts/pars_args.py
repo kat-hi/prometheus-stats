974d566e8bd09046ef5d845a5090e914202572ce
kat-hi <s77518@beuth-hochschule.de>
2020-03-21 18:58:49 +0100

    outsource parsing

--- scripts/pars_args.py
+++ scripts/pars_args.py
@@ -6,7 +6,4 @@ def parse_args():
     parser.add_argument('--host', type=str, required=False, default='localhost',
                         help='hostname of InfluxDB http API')
     parser.add_argument('--port', type=int, required=False, default=8086, help='port of InfluxDB http API')
-    log.info('PARSER: ' + str(parser.parse_args()))
-    log.info(parser.parse_args().host)
-    log.info(parser.parse_args().port)
     return parser.parse_args()
