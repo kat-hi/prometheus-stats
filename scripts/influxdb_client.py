96d8244db283cf924e289999a5278cda6590133e
kat-hi <s77518@beuth-hochschule.de>
2020-04-09 23:30:28 +0200

    refactoring: simplify and more generic

--- scripts/influxdb_client.py
+++ scripts/influxdb_client.py
@@ -3,120 +3,126 @@ from influxdb import DataFrameClient
 import matplotlib.pyplot as plt
 import seaborn as sns
 import logging as log
+import json
 
 INFLUX_VARS = ['up', 'ceph_osd_op_r', 'ceph_osd_op_w', 'ceph_osd_op_rw']
 
 log.basicConfig(level=log.INFO, format='%(message)s')
+resp = ""
+node_metrics = []
+cluster_metrics = []
+exception_metrics = []
+no_data_metrics = []
 
 import pars_args as parser
 
 class Influxfetcher:
     client = DataFrameClient(host=parser.parse_args().host, port=parser.parse_args().port, database='prometheus')
 
-    def __get_raw_data(self, predictor, colnames):
+    def __get_raw_data_from_single_var_as_dataframe(self, predictor):
         log.info('get predictor: ' + predictor)
 
         resp = self.client.query(
-            "select mean(%s) from %s WHERE time >= '2020-03-02T08:00:00.00Z' AND time <= '2020-03-12T20:00:00.00Z' "
-            "GROUP BY time(60s)" % (colnames[0], predictor))
+            "select mean(%s) from %s WHERE time >= '2020-03-01T08:00:00.00Z' AND time <= '2020-03-20T20:00:00.00Z' "
+            "GROUP BY time(10s)" % ('value', predictor))
 
         if not resp:
-            log.info('no data for predictor: ' + predictor + ' and queried colname: ' + colnames[0])
+            log.info('no data for predictor: ' + predictor + ' and queried colname: value')
         else:
             df = pd.DataFrame(resp[predictor], columns=['mean'])
             return df
 
-    def create_dataframe(self, influx_var):
-        df = self.__get_raw_data(influx_var, ['value'])
-        df.rename(columns={'mean': influx_var}, inplace=True)
+    def get_node_vs_cluster_metrics_as_csv(self):
+        global node_metrics, cluster_metrics, exception_metrics, no_data_metrics
+        metricfiles = ['cluster_metrics','node_metrics','exception_metrics','no_data_metrics']
+        with open('ressources/metrics.txt', 'r') as file:
+            f = file.read().split('\n')
+
+        global resp
+        for item in f:
+            try:
+                resp = self.client.query(
+                    "select * from %s WHERE time >= '2020-03-01T08:00:00.00Z' AND time <= '2020-03-01T08:01:00.00Z' " % item
+                )
+                if not resp:
+                    log.info('no data ' + item)
+                    no_data_metrics.append(item)
+                elif 'host' in resp[item]:
+                    log.info('host in  ' + item)
+                    node_metrics.append(item)
+                elif 'node' in resp[item]:
+                    log.info('node in ' + item)
+                    node_metrics.append(item)
+                else:
+                    log.info('cluster ' + item)
+                    cluster_metrics.append(item)
+            except:
+                log.info('except ' + item)
+                exception_metrics.append(item)
+
+        for metricgroup in metricfiles:
+            df = pd.DataFrame(metricgroup, columns=[metricgroup])
+            df.to_csv(metricgroup + '.csv', index=True, header=True)
+
+
+
+    def create_dataframes_with_value_col(self, variable):
+        df = self.__get_raw_data_from_single_var_as_dataframe(variable)
+        df.rename(columns={'mean': variable}, inplace=True)
         return df
 
 
-# TODO REFACTORING
-## UNTIL THEN, JUST RUN AND SEE:
     def dump_create_and_use_multiple_dataframes(self):
-        df_up = self.create_dataframe(INFLUX_VARS[0])
-        #df_ceph_osd_op_r = self.create_dataframe(INFLUX_VARS[1])
-        #df_ceph_osd_op_w = self.create_dataframe(INFLUX_VARS[2])
-        #df_ceph_osd_op_rw = self.create_dataframe(INFLUX_VARS[3])
+        df_up = self.create_dataframes_with_value_col(INFLUX_VARS[0])
+        df_ceph_osd_op_r = self.create_dataframes_with_value_col(INFLUX_VARS[1])
+        df_ceph_osd_op_w = self.create_dataframes_with_value_col(INFLUX_VARS[2])
+        df_ceph_osd_op_rw = self.create_dataframes_with_value_col(INFLUX_VARS[3])
 
 
         # DATA EXPLORATION WITH SINGLE VARIABLE
-        self.raw_data_exploration(df_up, INFLUX_VARS[0])
+        self.raw_data_exploration_single_variable(df_up, INFLUX_VARS[0])
         # self.raw_data_exploration(df_ceph_osd_op_r, INFLUX_VARS[1])
         # self.raw_data_exploration(df_ceph_osd_op_w, INFLUX_VARS[2])
         # self.raw_data_exploration(df_ceph_osd_op_rw, INFLUX_VARS[3])
 
-        # CONCAT DATAFRAMES TO ONE DATASET
-        #df_all = pd.concat([df_up, df_ceph_osd_op_r, df_ceph_osd_op_w, df_ceph_osd_op_rw], axis=1)
+        df_list = [df_up, df_ceph_osd_op_w, df_ceph_osd_op_r, df_ceph_osd_op_rw]
+        self.concat_and_clean_dataset(df_list)
 
-        # DATACLEANING
-        #df_all = df_all.dropna()
-
-        # DATA EXPLORATION OF CLEANED DATASET
         #for var in INFLUX_VARS:
         #    self.cleaned_dataset_exploration(df_all, var)
 
-        # DATAEXPLORATION step by step
-        ## PRINT MEAN; STD AND VAR
-        '''
-        print(str(df_up.values.mean()) + ' ' + str(df_up.values.std()) + ' ' + str(df_up.var()))
-        print(str(df_ceph_osd_op_r.values.mean()) + ' ' + str(df_ceph_osd_op_r.values.std()) + ' ' + str(
-            df_ceph_osd_op_r.var()))
-        print(str(df_ceph_osd_op_rw.values.mean()) + ' ' + str(df_ceph_osd_op_rw.values.std()) + ' ' + str(
-            df_ceph_osd_op_rw.var()))
-        print(str(df_ceph_osd_op_w.values.mean()) + ' ' + str(df_ceph_osd_op_w.values.std()) + ' ' + str(
-            df_ceph_osd_op_w.var()))
+        #for var in INFLUX_VARS:
+        #    self.dump_data_to_csv(df_all, var)
 
-        print(df_up.count())
-        '''
-
-        # DATAFRAMES TO CSV
-        '''
-        df_ceph_osd_op_rw.to_csv(r'../data/ceph_osd_op_rw.csv', index=True, header=True)
-        df_ceph_osd_op_r.to_csv(r'../data/ceph_osd_op_r.csv', index=True, header=True)
-        df_ceph_osd_op_w.to_csv(r'../data/ceph_osd_op_w.csv', index=True, header=True)
-        df_up.to_csv(r'../data/up.csv', index=True, header=True)
-        '''
-
-    '''
-    def get_all_prometheus_measurements():
+    def get_all_prometheus_measurements(self):
         all_measurements = []
-        response = client.get_list_measurements()
-        with open('../ressources/measurements.txt', 'w') as file:
+        response = self.client.get_list_measurements()
+        with open('../ressources/metrics.txt', 'w') as file:
             for item in response:
                 all_measurements.append(item['name'])
                 file.write(item['name'] + '\n')
-        # print(all_measurements)
-    '''
 
-    def raw_data_exploration(self, df, predictor):
-        log.info('\n predictor: ' + predictor)
-        # PLOTTING
 
-        log.info(df.head(10))
+    def concat_and_clean_dataset(self, df_list):
+        df_all = pd.concat(df_list, axis=1)
+        df_all = df_all.dropna()
+        return df_all
+
+    def get_simple_plot(self, df, predictor):
         if predictor is 'up':
             df['mean'].values[df['mean'] < 0.99] = 0
-
         axes = df['mean'].plot(marker='.', markersize=1, linewidth=0.00, alpha=0.9, color="g", figsize=(25, 4))
         axes.set_ylabel(predictor)
         axes.set_xlabel('time')
         plt.show()
 
+    def raw_data_exploration_single_variable(self, df, predictor):
+        log.info('\n predictor: ' + predictor)
+        self.get_simple_plot(df, predictor)
         df.rename(columns={'mean': predictor}, inplace=True)
 
-
         # DATAEXPLORATION
-        ## PRINT MEAN, STD AND VAR
-
-        log.info('mean:  ' + str(float(df.mean(axis=0))))
-        log.info('std: ' + str(float(df.std(axis=0))))
-        log.info('min: ' + str(float(df.min())))
-        log.info('max: ' + str(float(df.max())))
-        log.info('n: ' + str(float(df.count())))
-
-        # DATAFRAMES TO CSV
-        # df.to_csv(r'../data/' + predictor +'.csv', index=True, header=True)
+        log.info(df.describe())
 
         # df['date'] = df.index.date
         # df['day'] = df.index.day
@@ -124,13 +130,12 @@ class Influxfetcher:
         # df['index'] = df.index
         return df
 
-    def cleaned_dataset_exploration(self, df_all, var):
-        log.info('\n var: ' + var)
-        log.info('mean:  ' + str(float(df_all[var].mean(axis=0))))
-        log.info('std: ' + str(float(df_all[var].std(axis=0))))
-        log.info('min: ' + str(float(df_all[var].min())))
-        log.info('max: ' + str(float(df_all[var].max())))
-        log.info('n: ' + str(float(df_all[var].count())))
+    def dump_data_to_csv(self, df, variable):
+        df.to_csv(r'../data/' + variable +'.csv', index=True, header=True)
+
+    def clean_and_describe_dataset(self, df):
+        df = df.dropna()
+        log.info(df.describe())
 
 
 if __name__ == "__main__":
