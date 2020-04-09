b102f9033df8ae3fbb453186a0af0fd2e2ebb106
kat-hi <s77518@beuth-hochschule.de>
2020-04-09 23:31:04 +0200

    refactoring

--- scripts/logit.py
+++ scripts/logit.py
@@ -3,15 +3,13 @@ from influxdb_client import Influxfetcher, INFLUX_VARS
 import pandas as pd
 import numpy as np
 import logging as log
-import numpy as np
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
 import statsmodels.api as sm
 
 log.basicConfig(level=log.INFO, format='%(message)s')
 influxfetcher = Influxfetcher()
 
-def logit_model_estimation(df_all, var):
+def logit_model_one_predictor(df_all, var):
     # MODEL ESTIMATION
     log.info('LOGIT: up & ' + var )
     logit_model = sm.Logit(df_all['up'], df_all[var])
@@ -24,17 +22,12 @@ def logit_model_estimation(df_all, var):
 
 
 def logit_model_all(df_all):
-    log.info('LOGIT: up & r & w')
-    #predictors = df_all.drop(columns="up")
-    #predictors = predictors.drop(columns="ceph_osd_op_rw")
-    logit_model = sm.Logit(df_all['up'], df_all)
-
-    #logit_model = sm.Logit(df_all['up'], predictors)
+    predictors = df_all.drop(columns="up")
+    logit_model = sm.Logit(df_all['up'], predictors)
     result = logit_model.fit()
 
-    #log.info(result.summary())
-    #log.info(result.summary2())
-
+    log.info(result.summary())
+    log.info(result.summary2())
     get_odds(result)
 
 def get_odds(result):
@@ -46,25 +39,38 @@ def get_odds(result):
     conf.columns = ['2.5%', '97.5%', 'Odds']
     log.info(np.exp(conf))
 
+def estimate_logit_model(df_all):
+    for var in INFLUX_VARS:
+        if var is 'up':
+            continue
+        logit_model_one_predictor(df_all, var)
+
+def estimate_multiple_logit_model(df_all):
+    logit_model_all(df_all)
+
 
 if __name__ == "__main__":
-    df_up = influxfetcher.create_dataframe('up')
-    df_ceph_osd_op_r = influxfetcher.create_dataframe('ceph_osd_op_r')
-    df_ceph_osd_op_w = influxfetcher.create_dataframe('ceph_osd_op_w')
-    df_ceph_osd_op_rw = influxfetcher.create_dataframe('ceph_osd_op_rw')
+    df_up = influxfetcher.create_dataframes_with_value_col('up')
+    df_ceph_osd_op_r = influxfetcher.create_dataframes_with_value_col('ceph_osd_op_r')
+    df_ceph_osd_op_w = influxfetcher.create_dataframes_with_value_col('ceph_osd_op_w')
+    df_ceph_osd_op_rw = influxfetcher.create_dataframes_with_value_col('ceph_osd_op_rw')
+    df_node_ipmi_power_watts = influxfetcher.create_dataframes_with_value_col('node_ipmi_power_watts')
 
     df_up['up'].values[df_up['up'] < 0.99] = 0
     df_up['up'].values[df_up['up'] >= 0.99] = 1
 
-    df_all = pd.concat([df_up, df_ceph_osd_op_r, df_ceph_osd_op_w, df_ceph_osd_op_rw], axis=1)
-    df_all = df_all.dropna()
+    #df_all_cleaned = influxfetcher.concat_and_clean_dataset([df_up, df_ceph_osd_op_r, df_ceph_osd_op_w, df_ceph_osd_op_rw])
+    #influxfetcher.clean_and_describe_dataset(df_all_cleaned)
 
-    for var in INFLUX_VARS:
-        if var is 'up':
-            continue
-        logit_model_estimation(df_all, var)
+    #df_all = influxfetcher.concat_and_clean_dataset([df_up, df_node_ipmi_power_watts])
+    #influxfetcher.clean_and_describe_dataset(df_all)
 
-    logit_model_all(df_all)
+    #estimate_logit_model(df_all)
+    df_all = influxfetcher.concat_and_clean_dataset([df_up, df_ceph_osd_op_r, df_ceph_osd_op_w, df_ceph_osd_op_rw, df_node_ipmi_power_watts])
+    estimate_multiple_logit_model(df_all)
+
+    #influxfetcher.get_node_vs_cluster_metrics_as_csv()
+
 
 
 
